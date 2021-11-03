95a6362c06c43dc52030c8c992cb0511337b20f7
Aziz Ben Salem <azizbensalem1998@gmail.com>
2021-11-03 23:04:37 +0100

    retouche

--- public/assets/js/countdown-custom.js
+++ public/assets/js/countdown-custom.js
@@ -1,7 +1,7 @@
 (function($) {	
 	'use strict';
         jQuery(function () {
-            jQuery('#defaultCountdown').countdown({until: new Date(2021, 11-1, 28, 8)}); // year, month, date, hour
+            jQuery('#defaultCountdown').countdown({until: new Date(2021, 11-1, 29, 8)}); // year, month, date, hour
         });
 })(jQuery);
 
